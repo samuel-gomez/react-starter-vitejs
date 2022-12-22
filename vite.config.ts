b012a509ce74fb96fc44683863f503181b18c553
samuel.gomez <contact@samuel.gomez.fr>
2022-12-22 13:24:30 +0100

    feat(repo): repository config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  server: {
+    port: 3000,
+  },
   plugins: [react(), tsconfigPaths()],
   build: {
     outDir: 'build',
