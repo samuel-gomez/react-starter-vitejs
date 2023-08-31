367d499c6c2671637f7e2505f2b7c81a587c26b8
samuel.gomez <contact@samuel.gomez.fr>
2023-08-31 11:32:26 +0200

    feat(clean): update cleaned files

--- scripts/files/src/App/Routes/__tests__/Routes.test.tsx
+++ scripts/files/src/App/Routes/__tests__/Routes.test.tsx
@@ -1,104 +1,37 @@
-import { describe, it, expect, vi } from 'vitest';
-import { ComponentType, createContext } from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render, waitFor, act } from '@testing-library/react';
-import UserProvider from 'App/UserProvider';
-import { renderWithWrapperStaticRouter } from 'shared/testsUtils';
-import { TUserContext } from 'App/UserProvider/UserProvider';
-import Routes, { withAuth } from '..';
+import { describe, it, expect } from 'vitest';
+import { PropsWithChildren } from 'react';
+import { render, screen, waitFor, act } from 'shared/testsUtils/customRender';
 
-const defaultPropsMock = {
-  HomeCmpt: () => <div>HomeCmpt</div>,
-  PageUnauthorizeCmpt: () => <div>PageUnauthorizeCmpt</div>,
-};
+import { PROFILS } from 'shared/constants';
+import { TITLE as TITLE_HOME } from 'pages/Home';
+import { TITLE as TITLE_NOTFOUND } from 'pages/NotFound';
+import { TITLE as TITLE_UNAUTHORIZE } from 'pages/Unauthorize';
+import Routes, { ROUTE_URLS } from '..';
+import { RouteSecure } from '../RouteSecure';
 
-type TrenderRoute = {
-  role?: string;
-  name?: string;
-  route?: string;
-  defaultProps?: object;
-};
-
-const renderRoute = ({ role = '', name = '', route = '/', defaultProps = defaultPropsMock }: TrenderRoute) => {
-  const useOidcUser = vi.fn().mockReturnValue({
-    oidcUser: { name, member_of: [`CN=${role}`] },
-  });
-
-  return render(
-    <MemoryRouter initialEntries={[route]}>
-      <UserProvider useOidcUserFn={useOidcUser} isEnabled>
-        <Routes {...defaultProps} />
-      </UserProvider>
-    </MemoryRouter>,
-  );
-};
+const OidcSecureCmpt = ({ children }: PropsWithChildren) => <>{children}</>;
+const RouteSecureCmptMock = () => <RouteSecure OidcSecureCmpt={OidcSecureCmpt} />;
 
 describe('<Routes />', () => {
   it.each`
-    role  | name        | route
-    ${''} | ${'Samuel'} | ${'/'}
-    ${''} | ${'Samuel'} | ${'/forbidden'}
-  `('Should render page when user profil is authorized, role: $role, name: $name, route: $route', ({ role, name, route }) => {
-    const { asFragment } = renderRoute({ role, name, route });
-    expect(asFragment()).toMatchSnapshot();
+    role          | title                | route
+    ${PROFILS[0]} | ${TITLE_HOME}        | ${ROUTE_URLS.HOME}
+    ${'unknown'}  | ${TITLE_UNAUTHORIZE} | ${ROUTE_URLS.UNAUTHORIZE}
+    ${'unknown'}  | ${TITLE_NOTFOUND}    | ${ROUTE_URLS.NOTFOUND}
+  `('Should render page routes, role: $role, title: $title, route: $route', async ({ role, title, route }) => {
+    render(<Routes RouteSecureCmpt={RouteSecureCmptMock} />, {}, { role, route });
+    act(() => {
+      screen.getByText('Chargement de la page...');
+    });
+    await waitFor(() => expect(screen.getByText(title)).toBeInTheDocument());
   });
 
   it.each`
-    role                   | name     | route
-    ${'user unauthorized'} | ${'sam'} | ${'/'}
-  `('Should render forbidden page when user profil is unauthorized, role: $role, name: $name, route: $route', ({ role, name, route }) => {
-    const UserContextObjMock = createContext<TUserContext>({ authName: '', authRole: role, authUid: '', isEnabled: true, isLoading: false });
-    const { getByText } = renderRoute({
-      role,
-      name,
-      route,
-      defaultProps: { ...defaultPropsMock, withAuthFn: (...args: ComponentType<object>[]) => withAuth(args[0], UserContextObjMock, ['admin']) },
-    });
-    expect(getByText('PageUnauthorizeCmpt')).toBeInTheDocument();
-  });
+    role         | title                | route
+    ${'unknown'} | ${TITLE_UNAUTHORIZE} | ${ROUTE_URLS.HOME}
+  `('Should redirect to unauthorize page for protected routes, role: $role, title: $title, route: $route', async ({ role, title, route }) => {
+    render(<Routes RouteSecureCmpt={RouteSecureCmptMock} />, {}, { role, route });
 
-  it.each`
-    route
-    ${'/no-exist-route'}
-  `('Should render 404 when route: $route is not correct', async ({ route }) => {
-    const { getByText } = renderRoute({ route, role: '', name: 'samuel' });
-    await act(() => {
-      getByText('Chargement de la page...');
-    });
-
-    await waitFor(() => expect(getByText('404')).toBeInTheDocument());
-  });
-});
-
-describe('Render withAuth', () => {
-  const Component = () => <p>component</p>;
-  it.each`
-    authRole | authName
-    ${''}    | ${'Samuel'}
-  `('Should render RouteCmpt when user profile is authorized (authRole, authName)', ({ authRole, authName }) => {
-    const UserContextObjMock = createContext<TUserContext>({ authRole, authName, authUid: '', isEnabled: true, isLoading: false });
-    const { getByText } = renderWithWrapperStaticRouter(withAuth(Component, UserContextObjMock));
-    expect(getByText('component')).toBeInTheDocument();
-  });
-
-  it('Should render NavigateCmpt when user profile is unauthorized', () => {
-    const UserContextObjMock = createContext<TUserContext>({
-      authName: '',
-      authRole: 'unauthorized',
-      authUid: '',
-      isEnabled: true,
-      isLoading: false,
-    });
-    const NavigateCmpt = vi.fn().mockReturnValue(null);
-    renderWithWrapperStaticRouter(withAuth(Component, UserContextObjMock, [''], NavigateCmpt));
-    expect(NavigateCmpt).toHaveBeenCalled();
-  });
-
-  it('Should render LoaderCmpt when user profile is loading', () => {
-    const LoaderCmpt = vi.fn();
-    const UserContextObjMock = createContext<TUserContext>({ authName: '', authRole: '', authUid: '', isEnabled: true, isLoading: true });
-    const NavigateCmpt = vi.fn().mockReturnValue(null);
-    renderWithWrapperStaticRouter(withAuth(Component, UserContextObjMock, [''], NavigateCmpt, LoaderCmpt));
-    expect(LoaderCmpt).toHaveBeenCalled();
+    await waitFor(() => expect(screen.getByText(title)).toBeInTheDocument());
   });
 });
