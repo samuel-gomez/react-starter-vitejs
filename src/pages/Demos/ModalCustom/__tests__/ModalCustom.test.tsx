0f538e787836ae88983d26f03c2868109bc2de05
samuel.gomez <contact@samuel.gomez.fr>
2023-09-21 17:32:03 +0200

    test(modalcustom): clean tests

---
+++ src/pages/Demos/ModalCustom/__tests__/ModalCustom.test.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from 'shared/testsUtils';
+import { expectTitle } from 'shared/testsUtils/sharedScenarios';
+import ModalPageContainer from '../ModalCustom.container';
+
+const checkContent = () => {
+  expectTitle({ name: 'Custom Modal', level: 1 });
+
+  const button = screen.getByRole('button', { name: /Click me to launch modal/ });
+  expect(button).toBeInTheDocument();
+
+  return button;
+};
+
+describe('<ModalPageContainer/>', () => {
+  it('Should render Modal page with title and button', async () => {
+    render(<ModalPageContainer />);
+    expect(await screen.findByText(/Samuel/)).toBeInTheDocument();
+    checkContent();
+  });
+});
