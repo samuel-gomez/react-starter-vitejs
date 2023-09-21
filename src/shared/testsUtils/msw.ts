155da89ceecdd7475d5078aa34c36670bd4c4d37
samuel.gomez <contact@samuel.gomez.fr>
2023-09-21 17:33:09 +0200

    test(msw): add post response

--- src/shared/testsUtils/msw.ts
+++ src/shared/testsUtils/msw.ts
@@ -1,7 +1,7 @@
 import { rest } from 'msw';
 import type { ResponseResolver, RestRequest, PathParams, RestContext } from 'msw';
 import { setupServer } from 'msw/node';
-import { GITHUB_API } from 'shared/constants';
+import { GITHUB_API, STATUS_HTTP, STATUS_HTTP_MESSAGES } from 'shared/constants';
 import { MOCK_API_URL } from '.';
 
 type HandlerResolver = ResponseResolver<RestRequest<never, PathParams<string>>, RestContext>;
@@ -18,11 +18,38 @@ const commonResponse: HandlerResolver = (req, res, ctx) => {
   );
 };
 
+const commonPostResponse: HandlerResolver = async (req, res, ctx) => {
+  const testMock = req.headers.get('testMock');
+  const testMockParsed = testMock === null ? {} : JSON.parse(testMock);
+  const { code = 200 } = testMockParsed;
+  const { ...responseBody } = await req.json();
+
+  return `${code}` === '200'
+    ? res(
+        ctx.json({
+          code,
+          responseBody: {
+            ...responseBody,
+            id: '123',
+          },
+        }),
+      )
+    : res(
+        ctx.status(code),
+        ctx.json({
+          code,
+          statusHttp: code,
+          label: STATUS_HTTP_MESSAGES[STATUS_HTTP.SERVER_ERROR],
+        }),
+      );
+};
+
 export const handlers = [
   rest.get(`${MOCK_API_URL.github}${GITHUB_API}test/README.md`, commonResponse),
   rest.get(`${MOCK_API_URL.base}members`, commonResponse),
   rest.get(`${MOCK_API_URL.base}members/search`, commonResponse),
   rest.get(`${MOCK_API_URL.base}members/:id/download-detail`, commonResponse),
+  rest.post(`${MOCK_API_URL.base}members/add`, commonPostResponse),
 ];
 
 // This configures a request mocking server with the given request handlers.
