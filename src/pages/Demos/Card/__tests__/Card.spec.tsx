b0f89709262c6e9d6a4194f1545c60943124dc3c
Guillaume KESTEMAN <B519MA@login.axa>
2023-12-01 11:21:51 +0100

    feat(card demo): create card demo

---
+++ src/pages/Demos/Card/__tests__/Card.spec.tsx
@@ -0,0 +1,39 @@
+import { defineFeature, loadFeature } from 'jest-cucumber';
+import { SCOPE_PREVIEW } from 'shared/testsUtils/constants';
+import { configure, render, screen } from 'shared/testsUtils/customRender';
+import {
+  JeSuisUnUtilisateurConnuEtConnecteAvecleProfil,
+  UnBoutonEstVisible,
+  UnLienEstVisible,
+  UnTexteEstVisible,
+  UnTitreEstVisible,
+} from 'shared/testsUtils/sharedScenarios';
+import CardPage from '../Card';
+
+configure({ defaultHidden: true });
+
+const feature = loadFeature('features/Demos/Card/Card.feature');
+
+defineFeature(feature, test => {
+  let role: string;
+
+  const setRoleMock = (roleMock: string) => {
+    role = roleMock;
+  };
+
+  const renderPage = async () => {
+    render(<CardPage />, {}, { role });
+    expect(await screen.findByText(/Samuel/)).toBeInTheDocument();
+  };
+
+  test('Affichage du playground Card', ({ given, when, then, and }) => {
+    JeSuisUnUtilisateurConnuEtConnecteAvecleProfil(given, setRoleMock);
+    when('J’accède à la page démo Card', renderPage);
+    UnTitreEstVisible(then);
+    UnLienEstVisible(and);
+    UnLienEstVisible(and);
+    UnLienEstVisible(and);
+    UnBoutonEstVisible(and);
+    UnTexteEstVisible(and, SCOPE_PREVIEW);
+  });
+});
