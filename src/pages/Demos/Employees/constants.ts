1ffb669817609a998814a46352ea2b3ae27c9436
GuillaumeKESTEMAN <guillaume.kesteman@axa.fr>
2024-01-03 09:24:39 +0100

    feat(employees demo): use tableData.json in constants.tsx to avoid duplication

--- src/pages/Demos/Employees/constants.ts
+++ src/pages/Demos/Employees/constants.ts
@@ -1,7 +1,11 @@
+import tableData from './tableData.json';
+
 export const TITLE_BAR = 'Gestion des collaborateurs';
 export const TITLE = 'Liste des collaborateurs';
 export const ROUTE_URL_EMPLOYEES = 'employees';
 export const TABLE_ITEMS_TYPE = 'collaborateurs';
+
+export const SERVICE_NAME = 'employees';
 
 export const TABLE_HEADERS = [
   { id: 'header', key: 'header', isBlank: true },
@@ -9,61 +13,4 @@ export const TABLE_HEADERS = [
   { id: 'employee2', key: 'employee2', label: 'Collaborateur 2', field: 'employee2' },
 ];
 
-export const SERVICE_NAME = 'employees';
-
-export const TABLE_DATA = [
-  {
-    key: '1',
-    cols: {
-      header: {
-        id: 'contact',
-        label: 'Contact',
-        isHeader: true,
-      },
-      employee1: {
-        label: 'James Philips',
-        headers: 'employee1 contact',
-      },
-      employee2: {
-        label: 'Marie Beauchamp',
-        headers: 'employee2 contact',
-      },
-    },
-  },
-  {
-    key: '2',
-    cols: {
-      header: {
-        id: 'position',
-        label: 'Position',
-        isHeader: true,
-      },
-      employee1: {
-        label: 'Directeur des ventes',
-        headers: 'employee1 position',
-      },
-      employee2: {
-        label: 'Manager des ventes',
-        headers: 'employee2 position',
-      },
-    },
-  },
-  {
-    key: '3',
-    cols: {
-      header: {
-        id: 'email',
-        label: 'Email',
-        isHeader: true,
-      },
-      employee1: {
-        label: 'jp@1ltd.example.com',
-        headers: 'employee1 email',
-      },
-      employee2: {
-        label: 'marie@2co.example.com',
-        headers: 'employee2 email',
-      },
-    },
-  },
-];
+export const TABLE_DATA = Object.values(tableData);
