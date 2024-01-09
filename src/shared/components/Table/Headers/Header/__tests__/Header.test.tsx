b5cb092db6f09f550c520897e3bf8c05b1fed40c
GuillaumeKESTEMAN <guillaume.kesteman@axa.fr>
2024-01-09 09:50:06 +0100

    test(table header): remove unnecessary params

--- src/shared/components/Table/Headers/Header/__tests__/Header.test.tsx
+++ src/shared/components/Table/Headers/Header/__tests__/Header.test.tsx
@@ -1,33 +1,17 @@
 import { renderWithContainer } from 'shared/testsUtils';
 import { screen, within } from 'shared/testsUtils/customRender';
 import Header from '../Header';
-import { THeader } from '../type';
-
-const defaultProps = {
-  onSort: vi.fn(),
-};
 
 const container = document.createElement('tr');
 
-const headerMock: THeader = {
-  label: 'label',
-  key: 'uid',
-  field: 'name',
-};
-const sortingMock = {
-  field: 'name',
-  order: 1,
-};
-
 describe('Header', () => {
   it.each`
-    header        | sorting        | scope
-    ${headerMock} | ${undefined}   | ${undefined}
-    ${headerMock} | ${sortingMock} | ${'col'}
-    ${headerMock} | ${undefined}   | ${'row'}
-    ${headerMock} | ${sortingMock} | ${'row'}
-  `('Should render Header when header: $header, sorting: $sorting, scope: $scope', ({ header, sorting, scope }) => {
-    const { baseElement } = renderWithContainer(<Header scope={scope} label={header.label} sorting={sorting} {...defaultProps} />, container);
+    scope
+    ${undefined}
+    ${'col'}
+    ${'row'}
+  `('Should render Header when scope: $scope', ({ scope }) => {
+    const { baseElement } = renderWithContainer(<Header scope={scope} label="label" />, container);
 
     within(baseElement).getByRole(scope === 'col' || scope === undefined ? 'columnheader' : 'rowheader');
     screen.getByText('label');
