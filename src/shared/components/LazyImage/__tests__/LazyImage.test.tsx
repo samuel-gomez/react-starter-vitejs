008c9c1d5e3c12e928c669672e333025cc6c995b
samuel.gomez <contact@samuel.gomez.fr>
2024-02-21 09:27:41 +0100

    refactor(lazyimage): refactor lazy image

--- src/shared/components/LazyImage/__tests__/LazyImage.test.tsx
+++ src/shared/components/LazyImage/__tests__/LazyImage.test.tsx
@@ -1,5 +1,5 @@
-import { act, render, waitFor } from '@testing-library/react';
-import LasyImage, { loadImage } from '../LazyImage';
+import { act, render, screen, waitFor } from '@testing-library/react';
+import LasyImage, { DisplayImage, importImage, loadImage, setStateImageError, setStateImageLoaded, setStateImageSuccess } from '../LazyImage';
 
 const defaultProps = { alt: 'alt', name: '' };
 
@@ -11,7 +11,14 @@ describe('<LasyImage/>', () => {
     });
 
     await waitFor(() => expect(getByAltText('alt_accordion')).toBeDefined());
-    expect(asFragment()).toMatchSnapshot();
+    expect(asFragment()).toMatchInlineSnapshot(`
+      <DocumentFragment>
+        <img
+          alt="alt_accordion"
+          src="/public/images/accordion.svg"
+        />
+      </DocumentFragment>
+    `);
   });
 
   it('Should render LasyImage unknown image', async () => {
@@ -21,31 +28,134 @@ describe('<LasyImage/>', () => {
     });
 
     await waitFor(() => expect(getByAltText('alt_unknown')).toBeDefined());
-    expect(asFragment()).toMatchSnapshot();
+    expect(asFragment()).toMatchInlineSnapshot(`
+      <DocumentFragment>
+        <img
+          alt="alt_unknown"
+          src="/public/images/unknown.svg"
+        />
+      </DocumentFragment>
+    `);
   });
 
   it('Should render LasyImage when loading', () => {
     const { asFragment } = render(<LasyImage {...defaultProps} />);
-    expect(asFragment()).toMatchSnapshot();
+    expect(asFragment()).toMatchInlineSnapshot(`
+      <DocumentFragment>
+        <img
+          alt="loading..."
+          class="af-lasyimage"
+          src="/src/assets/puff.svg"
+        />
+      </DocumentFragment>
+    `);
+  });
+});
+
+describe('importImage', () => {
+  it('Should return import when importImage have been called with image name and extension', async () => {
+    const result = await importImage('quality', 'png');
+    expect(result.default).toEqual('/public/images/quality.png');
+  });
+
+  it('Should return import when importImage have been called with image name and without extension', async () => {
+    const result = await importImage('text');
+    expect(result.default).toEqual('/public/images/text.svg');
+  });
+
+  it('Should return import when importImage have been called with image unknown name and without extension', async () => {
+    try {
+      await importImage('unknown');
+    } catch (error) {
+      expect((error as Error).message).toEqual('Unknown variable dynamic import: ../../../../public/images/unknown.svg');
+    }
   });
 });
 
 describe('loadImage', () => {
-  it('Should return import when loadImage have been called with image name and extension', async () => {
-    const result = await loadImage('quality', 'png');
-    expect(result.default).toEqual('/public/images/quality.png');
+  const setStateImage = vi.fn();
+  const name = 'file.jpg';
+  const importImageFn = vi.fn();
+  const setStateImageSuccessFn = vi.fn();
+  const setStateImageErrorFn = vi.fn();
+  const setStateImageLoadedFn = vi.fn();
+
+  it('Should load image et set state success when importImage have resolved', async () => {
+    importImageFn.mockResolvedValue({ default: 'public/file.jpg' });
+    await loadImage({ setStateImage, name, importImageFn, setStateImageSuccessFn, setStateImageLoadedFn });
+
+    expect(importImageFn).toHaveBeenCalledWith('file', 'jpg');
+    expect(setStateImageSuccessFn).toHaveBeenCalledWith('public/file.jpg');
+    expect(setStateImageLoadedFn).toHaveBeenCalledWith(false);
   });
 
-  it('Should return import when loadImage have been called with image name and without extension', async () => {
-    const result = await loadImage('text');
-    expect(result.default).toEqual('/public/images/text.svg');
+  it('Should load image et set state error when importImage have rejected', async () => {
+    importImageFn.mockRejectedValue(new Error('erreur de chargement'));
+    await loadImage({ setStateImage, name, importImageFn, setStateImageErrorFn, setStateImageLoadedFn });
+
+    expect(importImageFn).toHaveBeenCalledWith('file', 'jpg');
+    expect(setStateImageErrorFn).toHaveBeenCalledWith(new Error('erreur de chargement'));
+    expect(setStateImageLoadedFn).toHaveBeenCalledWith(false);
+  });
+});
+
+describe('<DisplayImage />', () => {
+  it('Shoud render the image when image is defined', () => {
+    render(<DisplayImage image="image.jpg" alt="my image" />);
+    const img = screen.getByAltText('my image');
+
+    expect(img).toBeInTheDocument();
   });
 
-  it('Should return import when loadImage have been called with image unknown name and without extension', async () => {
-    try {
-      await loadImage('unknown');
-    } catch (error) {
-      expect((error as Error).message).toEqual('Unknown variable dynamic import: ../../../../public/images/unknown.svg');
-    }
+  it('Shoud render error message when image is not defined and error is defined', () => {
+    render(<DisplayImage alt="my image" error={{ message: 'erreur message', name: 'Error name' }} />);
+    const img = screen.getByText('erreur message');
+
+    expect(img).toBeInTheDocument();
+  });
+
+  it('Shoud render error message when image is not defined and error is not defined', () => {
+    render(<DisplayImage />);
+    const img = screen.getByText('Image non chargée');
+
+    expect(img).toBeInTheDocument();
+  });
+});
+
+describe('setStateImage functions', () => {
+  const prevState = {
+    image: null,
+    error: null,
+    loading: true,
+  };
+
+  it('Should return the new state with error', async () => {
+    const myError = new Error('my error');
+    const result = setStateImageError(myError)(prevState);
+    expect(result).toMatchObject({
+      image: null,
+      error: myError,
+      loading: true,
+    });
+  });
+
+  it('Should return the new state with image', async () => {
+    const myImage = 'myImage.jpg';
+    const result = setStateImageSuccess(myImage)(prevState);
+    expect(result).toMatchObject({
+      image: myImage,
+      error: null,
+      loading: true,
+    });
+  });
+
+  it('Should return the new state with loading', async () => {
+    const loading = false;
+    const result = setStateImageLoaded(loading)(prevState);
+    expect(result).toMatchObject({
+      image: null,
+      error: null,
+      loading,
+    });
   });
 });
